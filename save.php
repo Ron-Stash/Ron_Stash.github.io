90ddfe0f56018e3c9f1769fd21fff43a542020ad
Ron's Stash <139947897+Ron-Stash@users.noreply.github.com>
2023-08-06 11:48:23 +0800

    Delete save.php

--- save.php
+++
@@ -1,9 +0,0 @@
-// save.php
-$account = $_GET['account'];
-$password = $_GET['password'];
-
-// 数据校验
-
-$html = "<div>账号:$account</div><div>密码:$password</div>";
-
-file_put_contents('data.txt', $html);
